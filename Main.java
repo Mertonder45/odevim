eb8954789331a3e9e4ff8e91280c3d70398931a4
Mertonder45 <100041898+Mertonder45@users.noreply.github.com>
2023-11-05 03:52:43 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int yas=20;
+        String ad="mert";
+        String soyad="önder";
+        String sinifi="bilgisayar teknolojisi";
+        String memleket="AMASYA";
+
+        System.out.println(yas);
+        System.out.println(ad);
+        System.out.println(soyad);
+        System.out.println(sinifi);
+        System.out.println(memleket);
+
+
+    }
+}
