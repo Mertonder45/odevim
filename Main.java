fa64ea76dd3fe0d70aed8a2b85c5b3cf8a5f7fcf
Mertonder45 <100041898+Mertonder45@users.noreply.github.com>
2023-11-06 15:40:54 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,46 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int yas=20;
-        String ad="mert";
-        String soyad="önder";
-        String sinifi="bilgisayar teknolojisi";
-        String memleket="AMASYA";
+        Scanner scn =new Scanner(System.in);
+        System.out.println("isim giriniz : ");
+        String h = scn.nextLine();
+        System.out.println("HOŞGELDİNİZ"+h);
 
-        System.out.println(yas);
-        System.out.println(ad);
-        System.out.println(soyad);
-        System.out.println(sinifi);
-        System.out.println(memleket);
+        System.out.println("yaşınızı girin");
+        int yas =scn.nextInt();
+        if (yas > 18){
+            System.out.println("ehliyet alabilir");
 
+    }
+        else {
+            System.out.println("ehliiyet almak için yaş tutmuyor");
+        }
+        System.out.println("Sayı Giriniz");
+        int gun= scn.nextInt();
+        switch (gun){
+            case 1:
+                System.out.println("pazartesi");
+                break;
+            case 2:
+                System.out.println("salı");
+                break;
+            case 3:
+                System.out.println("çarşamba");
+                break;
+            case 4:
+                System.out.println("perşembe");
+                break;
+            case 5:
+                System.out.println("cuma");
+                break;
+            case 6:
+                System.out.println("cumartesi");
+                break;
+            case 7:
+                System.out.println("pazar");
+                break;
 
+        }
     }
 }
